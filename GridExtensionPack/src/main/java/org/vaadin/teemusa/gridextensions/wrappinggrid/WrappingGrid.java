4cbc93c55f5536676518b9c61003f6955b47724f
Tati Lund <tatu@vaadin.com>
2015-12-30 16:06:03 +0200

    Added Javadoc in comments

--- GridExtensionPack/src/main/java/org/vaadin/teemusa/gridextensions/wrappinggrid/WrappingGrid.java
+++ GridExtensionPack/src/main/java/org/vaadin/teemusa/gridextensions/wrappinggrid/WrappingGrid.java
@@ -7,13 +7,23 @@ import com.vaadin.ui.Grid;
 
 @SuppressWarnings("serial")
 public class WrappingGrid extends AbstractExtension {
-	
+
+	/**
+	 * Constructor to create header wrapping extension for given Grid
+	 * 
+	 * @param grid The Grid where you want to apply header wrapping to
+	 * @return
+	 */
 	public static WrappingGrid extend(Grid grid) {
 		WrappingGrid g = new WrappingGrid();
 		g.extend((AbstractClientConnector)grid);
 		return g;
 	}
-	
+
+	/**
+	 * 
+	 * @param enable Set header wrapping on/off true = on, false = off
+	 */
 	public void setWrapping(boolean enable) {
 		getRpcProxy(WrappingClientRPC.class).setWrapping(enable);
 	}
