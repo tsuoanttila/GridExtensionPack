9f0ce6c7cacf558a41b5d5634970ef8387cdaa21
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-12-31 09:35:06 +0200

    Fix SidebarMenuExtension with Grid in TabSheet (#9)

--- GridExtensionPack/src/main/java/org/vaadin/teemusa/gridextensions/client/sidebarmenuextension/SidebarMenuExtensionConnector.java
+++ GridExtensionPack/src/main/java/org/vaadin/teemusa/gridextensions/client/sidebarmenuextension/SidebarMenuExtensionConnector.java
@@ -28,91 +28,87 @@ import elemental.json.JsonObject;
 @Connect(SidebarMenuExtension.class)
 public class SidebarMenuExtensionConnector extends AbstractExtensionConnector {
 
-    Map<Integer, MenuItem> menuItemMap = new HashMap<Integer, MenuItem>();
-    Map<Integer, String> styleMap = new HashMap<Integer, String>();
+	Map<Integer, MenuItem> menuItemMap = new HashMap<Integer, MenuItem>();
+	Map<Integer, String> styleMap = new HashMap<Integer, String>();
+	Grid<JsonObject> grid;
 
-    private Grid<JsonObject> getGrid() {
-        return ((GridConnector) getParent()).getWidget();
-    }
+	private Grid<JsonObject> getGrid() {
+		return ((GridConnector) getParent()).getWidget();
+	}
 
-    @Override
-    protected void extend(ServerConnector target) {
-        // NOP
-    }
+	@Override
+	protected void extend(ServerConnector target) {
+		grid = getGrid();
+	}
 
-    @OnStateChange("captionMap")
-    private void captionMapChanged() {
-        MenuBar sidebarMenu = getGrid().getSidebarMenu();
-        for (Entry<Integer, MenuItem> entry : new HashSet<Entry<Integer, MenuItem>>(
-                menuItemMap.entrySet())) {
-            sidebarMenu.removeItem(entry.getValue());
-            if (!getState().captionMap.containsKey(entry.getKey())) {
-                menuItemMap.remove(entry.getKey());
-                styleMap.remove(entry.getKey());
-            }
-        }
-        for (Entry<Integer, String> entry : getState().captionMap.entrySet()) {
-            MenuItem menuItem;
-            if (menuItemMap.containsKey(entry.getKey())) {
-                menuItem = menuItemMap.get(entry.getKey());
-                menuItem.setText(entry.getValue());
-            } else {
-                menuItem = createMenuItem(entry.getKey(), entry.getValue());
-            }
-            menuItemMap.put(entry.getKey(), menuItem);
-            if (styleMap.containsKey(entry.getKey())) {
-                menuItem.addStyleName(styleMap.get(entry.getKey()));
-            }
-            sidebarMenu.addItem(menuItem);
-        }
-    }
+	@OnStateChange("captionMap")
+	private void captionMapChanged() {
+		MenuBar sidebarMenu = grid.getSidebarMenu();
+		for (Entry<Integer, MenuItem> entry : new HashSet<Entry<Integer, MenuItem>>(menuItemMap.entrySet())) {
+			sidebarMenu.removeItem(entry.getValue());
+			if (!getState().captionMap.containsKey(entry.getKey())) {
+				menuItemMap.remove(entry.getKey());
+				styleMap.remove(entry.getKey());
+			}
+		}
+		for (Entry<Integer, String> entry : getState().captionMap.entrySet()) {
+			MenuItem menuItem;
+			if (menuItemMap.containsKey(entry.getKey())) {
+				menuItem = menuItemMap.get(entry.getKey());
+				menuItem.setText(entry.getValue());
+			} else {
+				menuItem = createMenuItem(entry.getKey(), entry.getValue());
+			}
+			menuItemMap.put(entry.getKey(), menuItem);
+			if (styleMap.containsKey(entry.getKey())) {
+				menuItem.addStyleName(styleMap.get(entry.getKey()));
+			}
+			sidebarMenu.addItem(menuItem);
+		}
+	}
 
-    @OnStateChange("styleMap")
-    private void styleMapChanged() {
-        for (Entry<Integer, String> entry : new HashSet<Entry<Integer, String>>(
-                styleMap.entrySet())) {
-            if (menuItemMap.containsKey(entry.getKey())) {
-                menuItemMap.get(entry.getKey())
-                        .removeStyleName(entry.getValue());
-            }
-            if (!getState().styleMap.containsKey(entry.getKey())
-                    || !getState().styleMap.get(entry.getKey())
-                            .equals(entry.getValue())) {
-                styleMap.remove(entry.getKey());
-            }
-        }
-        for (Entry<Integer, String> entry : getState().styleMap.entrySet()) {
-            styleMap.put(entry.getKey(), entry.getValue());
-            if (!menuItemMap.containsKey(entry.getKey())) {
-                MenuItem menuItem = createMenuItem(entry.getKey(),
-                        getState().captionMap.get(entry.getKey()));
-                menuItemMap.put(entry.getKey(), menuItem);
-            }
-            menuItemMap.get(entry.getKey()).addStyleName(entry.getValue());
-        }
-    }
+	@OnStateChange("styleMap")
+	private void styleMapChanged() {
+		for (Entry<Integer, String> entry : new HashSet<Entry<Integer, String>>(styleMap.entrySet())) {
+			if (menuItemMap.containsKey(entry.getKey())) {
+				menuItemMap.get(entry.getKey()).removeStyleName(entry.getValue());
+			}
+			if (!getState().styleMap.containsKey(entry.getKey())
+					|| !getState().styleMap.get(entry.getKey()).equals(entry.getValue())) {
+				styleMap.remove(entry.getKey());
+			}
+		}
+		for (Entry<Integer, String> entry : getState().styleMap.entrySet()) {
+			styleMap.put(entry.getKey(), entry.getValue());
+			if (!menuItemMap.containsKey(entry.getKey())) {
+				MenuItem menuItem = createMenuItem(entry.getKey(), getState().captionMap.get(entry.getKey()));
+				menuItemMap.put(entry.getKey(), menuItem);
+			}
+			menuItemMap.get(entry.getKey()).addStyleName(entry.getValue());
+		}
+	}
 
-    private MenuItem createMenuItem(final int id, String caption) {
-        return new MenuItem(caption, new ScheduledCommand() {
+	private MenuItem createMenuItem(final int id, String caption) {
+		return new MenuItem(caption, new ScheduledCommand() {
 
-            @Override
-            public void execute() {
-                getRpcProxy(SidebarMenuExtensionServerRpc.class).click(id);
-            }
-        });
-    }
+			@Override
+			public void execute() {
+				getRpcProxy(SidebarMenuExtensionServerRpc.class).click(id);
+			}
+		});
+	}
 
-    @Override
-    public SidebarMenuExtensionState getState() {
-        return (SidebarMenuExtensionState) super.getState();
-    }
+	@Override
+	public SidebarMenuExtensionState getState() {
+		return (SidebarMenuExtensionState) super.getState();
+	}
 
-    @Override
-    public void onUnregister() {
-        for (MenuItem menuItem : menuItemMap.values()) {
-            getGrid().getSidebarMenu().removeItem(menuItem);
-        }
+	@Override
+	public void onUnregister() {
+		for (MenuItem menuItem : menuItemMap.values()) {
+			grid.getSidebarMenu().removeItem(menuItem);
+		}
 
-        super.onUnregister();
-    }
+		super.onUnregister();
+	}
 }
